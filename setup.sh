b31852b300b4afa2cb3c5abb80a6c0cc3015789a
artyom <you@example.com>
2020-04-21 17:19:24 +0300

    change repo

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 apt install git
 apt install ansible
-git clone https://allmyrestprojects@bitbucket.org/allmyrestprojects/ansible.git ansible
+git clone https://github.com/arptra/roger-skyline-1.git ansible
 ansible-playbook -i /home/deb/ansible/hosts /home/deb/ansible/setup.yml --user=deb --extra-vars "ansible_sudo_pass=school21"
 
